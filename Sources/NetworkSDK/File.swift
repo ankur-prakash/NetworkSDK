aa9b55b2bbc77bbd421e46a2546e120028228877
Ankur Prakash <ankur.prakash@mavenir.com>
2022-07-02 06:32:37 +0530

    Add new class

---
+++ Sources/NetworkSDK/File.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ankur Prakash on 02/07/22.
+//
+
+import Foundation
+
+public class LoadManager {
+    
+    public init() {}
+    
+    public func printName() {
+        print("Sonu")
+    }
+}
